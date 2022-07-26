58203d76cc701ab3e9a2e6742ae797dfc9297785
Sszyh <zhuyer@outlook.com>
2022-07-26 12:28:52 -0700

    fix the problem of making sound in M1

--- timer1.js
+++ timer1.js
@@ -1,11 +1,11 @@
 
 const args = process.argv.slice(2)
-process.stdout.write('\x07');
 
 for (let elm of args) {
   if (elm > 0 && elm != "") {
     setTimeout(() => {
       console.log(`${elm} seconds`);
+      process.stdout.write('\007');
     }, elm * 1000);
   }
 }
